cf75bdbfaacd314e39a1a078277aed7fb17790c8
chris911e <90184668+chris911e@users.noreply.github.com>
2022-02-02 21:49:00 +0100

    Added 2nd button function and buffened shop

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -21,7 +21,7 @@ function autocliccUpgrade(){
         how_many_c_you_do_per_clicc += btn_autocliccer_boost;
         btn_autocliccer_count++;
         btn_autocliccer_boost *= 2;
-        btn_autocliccer_price *= 4;
+        btn_autocliccer_price *= 8;
         document.getElementById("counter").innerText = `You currently own ${cliccs} cliccs`;
         document.getElementById("how_may_c_you_do_per_clicc").innerText = `You currently do ${how_many_c_you_do_per_clicc}C per clicc`;
         document.getElementById("btn-autocliccer").innerText = `Buy AutoCliccer ${btn_autocliccer_price}C (+${btn_autocliccer_boost}C per clicc)`;
@@ -36,5 +36,14 @@ let btn_cliccyguy_price = 650;
 function cliccyguyUpgrade() {
     if (cliccs < btn_cliccyguy_price) {
         alert("Not enough cliccs :(");
+    } else {
+        cliccs -= btn_cliccyguy_price;
+        how_many_c_you_do_per_clicc += btn_cliccyguy_boost;
+        btn_cliccyguy_count++;
+        btn_cliccyguy_boost *= 3;
+        btn_cliccyguy_price *= 18;
+        document.getElementById("counter").innerText = `You currently own ${cliccs} cliccs`;
+        document.getElementById("how_may_c_you_do_per_clicc").innerText = `You currently do ${how_many_c_you_do_per_clicc}C per clicc`;
+        document.getElementById("btn-cliccyguy").innerText = `Buy CliccyGuy ${btn_cliccyguy_price}C (+${btn_cliccyguy_boost}C per clicc)`;
     }
 }
