37810aab1e6989f0f4f3a0487b359a1b0bda8ef8
Christopher Scheel <christopherscheel99@gmail.com>
2022-02-02 15:55:25 +0100

    Made separate files

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,46 +3,6 @@ let real_cliccs = 0; let cliccs = 0; let cliccer = 1; let booster = 1; cliccer *
 function clicc() {
     cliccs += cliccer;
     real_cliccs++;
-    document.getElementById("counter").innerText = cliccs + " Cliccs";
+    document.getElementById("counter").innerText = "You currently own " + cliccs + " cliccs";
     document.getElementById("real_cliccs").innerText = "You have clicced this button " + real_cliccs + " times"
 }
-
-const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
-
-const numBalls = 50;
-const balls = [];
-
-for (let i = 0; i < numBalls; i++) {
-    let ball = document.createElement("div");
-    ball.classList.add("ball");
-    ball.style.background = colors[Math.floor(Math.random() * colors.length)];
-    ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
-    ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
-    ball.style.transform = `scale(${Math.random()})`;
-    ball.style.width = `${Math.random()}em`;
-    ball.style.height = ball.style.width;
-    balls.push(ball);
-    document.body.append(ball);
-}
-
-
-balls.forEach((el, i, ra) => {
-    let to = {
-        x: Math.random() * (i % 2 === 0 ? -11 : 11),
-        y: Math.random() * 12
-    };
-
-    let anim = el.animate(
-        [
-        { transform: "translate(0, 0)" },
-        { transform: `translate(${to.x}rem, ${to.y}rem)` }
-        ],
-        {
-        duration: (Math.random() + 1) * 2000,
-        direction: "alternate",
-        fill: "both",
-        iterations: Infinity,
-        easing: "ease-in-out"
-        }
-    );
-});
