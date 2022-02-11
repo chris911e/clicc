792a7adb33dc9c1cd59c26712c4801bf1da885ae
chris911e <90184668+chris911e@users.noreply.github.com>
2022-02-11 11:25:07 +0100

    added cliccfactory

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,4 +1,4 @@
-let real_cliccs = 0; let cliccs = 0; let how_many_c_you_do_per_clicc = 1;
+let real_cliccs = 0; let cliccs = 1110; let how_many_c_you_do_per_clicc = 1;
 
 function clicc() {
     cliccs += how_many_c_you_do_per_clicc;
@@ -45,5 +45,25 @@ function cliccyguyUpgrade() {
         document.getElementById("counter").innerText = `You currently own ${cliccs} cliccs`;
         document.getElementById("how_may_c_you_do_per_clicc").innerText = `You currently do ${how_many_c_you_do_per_clicc}C per clicc`;
         document.getElementById("btn-cliccyguy").innerText = `Buy CliccyGuy ${btn_cliccyguy_price}C (+${btn_cliccyguy_boost}C per clicc)`;
+    }
+}
+
+// clicc factory
+
+let btn_cliccfactory_count = 0;
+let btn_cliccfactory_boost = 100;
+let btn_cliccfactory_price = 3250;
+function cliccfactoryUpgrade() {
+    if (cliccs < btn_cliccfactory_price) {
+        alert("Not enough cliccs :(")
+    } else {
+        cliccs -= btn_cliccfactory_price;
+        how_many_c_you_do_per_clicc += btn_cliccfactory_boost;
+        btn_cliccfactory_count++;
+        btn_cliccfactory_boost *= 4;
+        btn_cliccfactory_price *= 32;
+        document.getElementById("counter").innerText = `You currently own ${cliccs} cliccs`;
+        document.getElementById("how_may_c_you_do_per_clicc").innerText = `You currently do ${how_many_c_you_do_per_clicc}C per clicc`;
+        document.getElementById("btn-cliccfactory").innerText = `Buy CliccFactory ${btn_cliccfactory_price}C (+${btn_cliccfactory_boost}C per clicc)`;
     }
 }
